99d76cef4528b23ff7211aed1621b261308209c0
APTECH-MSG\lab-201 <aptechsheh@gmail.com>
2022-12-06 23:45:13 +0500

    added style

---
+++ A.O Hospital.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Covido</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+       <link rel="stylesheet" href="css/owl.carousel.min.css"> 
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <link rel="stylesheet" href="https://rawgit.com/LeshikJanz/libraries/master/Bootstrap/baguetteBox.min.css">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout inner_page">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#"/></div>
+      </div>
+      <!-- end loader -->
+      <!-- top -->
+                    <!-- header -->
+         <header class="header-area">
+            <div class="left">
+               <a href=""><i class="fa fa-search" aria-hidden="true"></i></a>
+            </div>
+            <div class="right">
+               <a href="u_page.php"><i class="fa fa-user" aria-hidden="true"></i></a>
+            </div>
+            <div class="container">
+               <div class="row d_flex">
+                  <div class="col-sm-3 logo_sm">
+                     <div class="logo">
+                        <a href="index.php"></a>
+                     </div>
+                  </div>
+                  <div class="col-lg-10 offset-lg-1 col-md-12 col-sm-9">
+                     <div class="navbar-area">
+                        <nav class="site-navbar">
+                        <ul>
+                              <li><a href="index.php">Home</a></li>
+                              <li><a href="about.php">About</a></li>
+                              <li><a href="action.php">take action</a></li>
+                              <!-- <li><a href="index.php" class="logo_midle">covido</a></li> -->
+                              <li><a class="active" href="news.php">news</a></li>
+                              <li><a href="doctores.php">doctores</a></li>
+                              <li><a href="contact.php">Contact </a></li>
+                           </ul>
+                           <button class="nav-toggler">
+                           <span></span>
+                           </button>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </header>
+      <!-- end header -->
+<body>
+</div>
+            <div class="container">
+               <div class="row">
+    <div class="col-lg-8 col-md-2" style=" height: 870px;">
+<img src="images/al khidmat.webp" width="50%;">
+        <br>
+    <div class="col-lg-10 col-md-10"  style=" border:2px solid #F5B7B1; box-shadow: 0 3px 10px rgb(0 0 0 / 0.2); width: 1000px; margin-left: 400px; margin-top:100px; border-radius: 20px;">
+     <br>
+        <h1><u><center>A.O Hospital</center></u></h1>
+    
+<br>
+<br>
+<div style="margin-left:30px;">
+     <h2>Contact Number: 0315-9824600</h2>
+     <br>
+     <h2>Location: Gulistan E Johar </h2>
+     <br>
+     <h2>Consultants: Dr ALi , Dr Parveen, Dr Faiq</h2>
+     <br>
+     <h2>Covid Vaccine: Sinovac , Fizer </h2>
+     <br>
+     <h2>Timing: 8 am To 2 am</h2>
+     <br>
+     <h2> 24 Hours Emergency Ward</h2>
+     <br>
+     <br>   
+    <center><button class="btn" style="border: 2px solid; background-color:gray; height: 55px;"> PATIENT DETAILS </button> </center>
+     <br>
+     <br>
+</div>
+    </div>
+     </div>
+     </div>
+        </div>
+ <!--  footer -->
+ <footer>
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                        <div class="col-lg-2 col-md-6 col-sm-6">
+                           <div class="hedingh3 text_align_left">
+                              <h3>Resources</h3>
+                              <ul class="menu_footer">
+                                 <li><a href="index.php">Home</a><li>
+                                 <li><a href="">What we do</a><li>
+                                 <li> <a href="">Media</a><li>
+                                 <li> <a href="">Travel Advice</a><li>
+                                 <li><a href="">Protection</a><li>
+                                 <li><a href="">Care</a><li>
+                              </ul>
+                             
+           
+                           </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                           <div class="hedingh3 text_align_left">
+                             <h3>About</h3>
+                              <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various</p>
+                           </div>
+                        </div>
+                     
+                
+                       
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                           <div class="hedingh3  text_align_left">
+                              <h3>Contact  Us</h3>
+                                <ul class="top_infomation">
+                        <li><i class="fa fa-map-marker" aria-hidden="true"></i>
+                           Making this the first true  
+                        </li>
+                        <li><i class="fa fa-phone" aria-hidden="true"></i>
+                           Call : +01 1234567890
+                        </li>
+                        <li><i class="fa fa-envelope" aria-hidden="true"></i>
+                           <a href="Javascript:void(0)">Email : demo@gmail.com</a>
+                        </li>
+                     </ul>
+                            
+                           
+                     </div>
+                  </div>
+                     <div class="col-lg-4 col-md-6 col-sm-6">
+                           <div class="hedingh3 text_align_left">
+                              <h3>countrys</h3>
+                              <div class="map">
+                                <img src="images/map.png" alt="#"/>
+                              </div>
+                           </div>
+                        </div>
+                    
+               </div>
+            </div>
+            <div class="copyright">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-8 offset-md-2">
+                        <p>© 2020 All Rights Reserved. Design by <a href="https://html.design/"> Free html Templates</a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/custom.js"></script>
+   </body>
+</html>
+    </body>
+</head>
+</html>
---
+++ hospitals.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Covido</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+       <link rel="stylesheet" href="css/owl.carousel.min.css"> 
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <link rel="stylesheet" href="https://rawgit.com/LeshikJanz/libraries/master/Bootstrap/baguetteBox.min.css">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout inner_page">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#"/></div>
+      </div>
+      <!-- end loader -->
+      <!-- top -->
+                    <!-- header -->
+         <header class="header-area">
+            <div class="left">
+               <a href=""><i class="fa fa-search" aria-hidden="true"></i></a>
+            </div>
+            <div class="right">
+               <a href="u_page.php"><i class="fa fa-user" aria-hidden="true"></i></a>
+            </div>
+            <div class="container">
+               <div class="row d_flex">
+                  <div class="col-sm-3 logo_sm">
+                     <div class="logo">
+                        <a href="index.php"></a>
+                     </div>
+                  </div>
+                  <div class="col-lg-10 offset-lg-1 col-md-12 col-sm-9">
+                     <div class="navbar-area">
+                        <nav class="site-navbar">
+                        <ul>
+                              <li><a href="index.php">Home</a></li>
+                              <li><a href="about.php">About</a></li>
+                              <li><a href="action.php">take action</a></li>
+                              <!-- <li><a href="index.php" class="logo_midle">covido</a></li> -->
+                              <li><a class="active" href="news.php">news</a></li>
+                              <li><a href="doctores.php">doctores</a></li>
+                              <li><a href="contact.php">Contact </a></li>
+                           </ul>
+                           <button class="nav-toggler">
+                           <span></span>
+                           </button>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </header>
+      <!-- end header -->
+
+<div class="container-fluid mt-5 text-center" >
+
+  
+        <h1 style="font-size:4em; text-align: center; color: black; margin-top:10px ;">HOSPITALS</h1>
+        <br>
+        <br>
+        <br>
+         <div class="card-deck">
+            <div class="card">
+              <img src="images/al khidmat.webp" class="" height="300px" width="492px" alt="..." style="margin-left: 0px;">
+              <div class="card-body">
+                <h1 class="card-title" style="font-size: 3em; text-align: center;">Al Khidmat Hospital</h1>
+             
+                <button class="btn" style="border: 2px solid; background-color:skyblue; color:white;  margin-top: 15px; height: 55px; width: 150px;"><a href="Al Khidmat.php">DETAILS </a></button>
+              </div>
+            </div>
+            <div class="card">
+              <img src="images/ao-hospital.jpg" class="" height="300px" width="570px" alt="..." style="margin-left: 0px;">
+              <div class="card-body">
+                <h1 class="card-title" style="font-size: 3em; text-align: center;">A.0 Hospital</h1>
+                
+                <button class="btn" style="border: 2px solid; background-color:skyblue; color:white;  margin-top: 15px;  height: 55px; width: 150px;"> <a href="A.O Hospital.php"> DETAILS </a></button>
+              
+              </div>
+            </div>
+            <div class="card">
+              <img src="images/pak international hospital.jpg" class="" height="400px" width="570px" alt="..." style="margin-left: 0px;">
+              <div class="card-body">
+                <h1 class="card-title" style="font-size: 2.5em; text-align: center;">Pak Internattional Hospital</h1>
+                <button class="btn" style="border: 2px solid; background-color:skyblue; color:white;  margin-top: 15px;  height: 55px; width: 150px;">DETAILS </button>
+              
+              </div>
+            </div>
+          </div>
+          <br>
+            <br>
+          <div class="card-deck">
+            <div class="card">
+              <img src="images/patel hospital.jpg" class="" height="300px" width="570px" alt="..." style="margin-left: 0px;">
+              <div class="card-body">
+                <h1 class="card-title" style="font-size: 3em; text-align: center;">Patel Hospital</h1>
+                
+                <button class="btn" style="border: 2px solid; background-color:skyblue; color:white;  margin-top:15px; height: 55px; width: 150px;">DETAILS </button>
+              
+              </div>
+            </div>
+            <div class="card">
+              <img src="images/shifa.jpg" class="" height="300px" width="570px" alt="..." style="margin-left: 0px;">
+              <div class="card-body">
+                <h1 class="card-title" style="font-size: 3em; text-align: center;">Shifa Hospital</h1>
+                
+                <button class="btn" style="border: 2px solid; background-color:skyblue; color:white;  margin-top: 15px;  height: 55px; width: 150px;">DETAILS </button>
+              
+              </div>
+            </div>
+            <div class="card">
+              <img src="images/children hospital.jpg" class="" height="300px" width="570px" alt="..." style="margin-left: 0px;">
+              <div class="card-body">
+                <h1 class="card-title" style="font-size: 2.8em; text-align: center;">Children Hospital</h1>
+               
+                <button class="btn" style="border: 2px solid; background-color:skyblue; color:white;  margin-top: 15px;  height: 55px; width: 150px;">DETAILS </button>
+              
+              </div>
+            </div> 
+            </div>
+            <br>
+            <br>
+            <div class="card-deck">
+              <div class="card">
+                <img src="images/agha khan.jpg" class="" height="300px" width="570px" alt="..." style="margin-left: 0px;">
+                <div class="card-body">
+                  <h1 class="card-title" style="font-size: 2.5em; text-align: center;">Agha Khan Hospital</h1>
+                  
+                  <button class="btn" style="border: 2px solid; background-color:skyblue; color:white;  margin-top: 15px;  height: 55px; width: 150px;">DETAILS </button>
+              
+                </div>
+              </div>
+              <div class="card">
+                <img src="images/dow hospital.jpg" class="" height="300px" width="570px" alt="..." style="margin-left: 0px;">
+                <div class="card-body">
+                  <h1 class="card-title" style="font-size: 3em; text-align: center;">Dow Hospital</h1>
+                  
+                  <button class="btn" style="border: 2px solid; background-color:skyblue; color:white;  margin-top: 15px;  height: 55px; width: 150px;">DETAILS </button>
+              
+                </div>
+              </div>
+              <div class="card">
+                <img src="images/liaquat national.jpg" class="" height="300px" width="600px" alt="..." style="margin-left: 0px;">
+                <div class="card-body">
+                  <h1 class="card-title" style="font-size: 2.5em; text-align: center;">Liaquat National Hospital</h1>
+                 
+                  <button class="btn" style="border: 2px solid; background-color:skyblue; color:white;  margin-top: 15px;  height: 55px; width: 150px;">DETAILS </button>
+                </div>
+          </div>
+        
+          </div>
+          </div>
+          
+         <!--  footer -->
+ <footer>
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                        <div class="col-lg-2 col-md-6 col-sm-6">
+                           <div class="hedingh3 text_align_left">
+                              <h3>Resources</h3>
+                              <ul class="menu_footer">
+                                 <li><a href="index.php">Home</a><li>
+                                 <li><a href="">What we do</a><li>
+                                 <li> <a href="">Media</a><li>
+                                 <li> <a href="">Travel Advice</a><li>
+                                 <li><a href="">Protection</a><li>
+                                 <li><a href="">Care</a><li>
+                              </ul>
+                             
+           
+                           </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                           <div class="hedingh3 text_align_left">
+                             <h3>About</h3>
+                              <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various</p>
+                           </div>
+                        </div>
+                     
+                
+                       
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                           <div class="hedingh3  text_align_left">
+                              <h3>Contact  Us</h3>
+                                <ul class="top_infomation">
+                        <li><i class="fa fa-map-marker" aria-hidden="true"></i>
+                           Making this the first true  
+                        </li>
+                        <li><i class="fa fa-phone" aria-hidden="true"></i>
+                           Call : +01 1234567890
+                        </li>
+                        <li><i class="fa fa-envelope" aria-hidden="true"></i>
+                           <a href="Javascript:void(0)">Email : demo@gmail.com</a>
+                        </li>
+                     </ul>
+                            
+                           
+                     </div>
+                  </div>
+                     <div class="col-lg-4 col-md-6 col-sm-6">
+                           <div class="hedingh3 text_align_left">
+                              <h3>countrys</h3>
+                              <div class="map">
+                                <img src="images/map.png" alt="#"/>
+                              </div>
+                           </div>
+                        </div>
+                    
+               </div>
+            </div>
+            <div class="copyright">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-8 offset-md-2">
+                        <p>© 2020 All Rights Reserved. Design by <a href="https://html.design/"> Free html Templates</a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/custom.js"></script>
+  
+    </body>
+
+</html>
